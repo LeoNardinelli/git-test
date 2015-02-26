6fa41a766848f1cec60791b6173f8a53ded2222a
41415711 <41415711@089978-P33S13.macklabs.br>
2015-02-26 11:47:13 -0300

    algoritmo para ver se aluno está aprovado ou reprovado

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 <html>
 
   <head>
@@ -11,9 +5,55 @@
     <meta charset="UTF-8">
         
   </head>
-     <body>   
-        
-  echo 12314;
-  echo <h1> 'Leonardo'; </h1>
-     </body>
-?>
+       
+         
+              
+         
+         <?php
+         
+         $p1=9.0*30; 
+              $m1=8.0*10;
+              $m2=7.5*10;
+              $proj1=6.0*5; 
+              $proj2=10.0*5; 
+              $proj3=8.0*5; 
+              $proj4=8.5*5;
+              $trabf=9.0*10;
+              $proc=6.5*20;
+              
+              $MF;
+              $PF=9.0;
+              
+              $MI=($p1+$m1+$m2+$proj1+$proj2+$proj3+$proj4+$trabf+$proc)/100;
+              
+              if ($MI>=7.5) {
+                  
+                  echo ' O aluno esta aprovado ';
+              }
+              else    {
+              $MF=($MI+$PF)/2;
+              }
+              if ($MF>=5) {
+                  echo 'O aluno está aprovado';
+                  
+              }
+              
+              else {
+                  echo 'O aluno esta reprovado';
+              }
+  
+         
+         
+         
+         
+         
+         
+         
+         
+         echo 12314;
+  echo '<h1> Leonardo; </h1>'
+  ?>
+     
+     
+</html>
+
