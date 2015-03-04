cf020af1a311036e5a2cceb510eb69f376dc740f
41415711 <41415711@089977-P33S13.macklabs.br>
2015-03-04 10:15:35 -0300

    Exercício Números Primos

---
+++ primos.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Número Primo</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>        
+<?php
+
+$numero=10;
+$cont=0;
+  if ($numero%2==0) {
+    $cont++;          
+  }
+  if ($cont==2){
+  echo 'O número é primo';}
+  
+  else { echo 'O número não é primo';}
+ ?>
+    </body>
+</html>
+
+    
+
+            
+
