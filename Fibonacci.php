3dcc2f15af05a32cfc1c16cbca861202b615f312
41415711 <41415711@089977-P33S13.macklabs.br>
2015-03-04 10:15:10 -0300

    Exercício Fibonacci

---
+++ Fibonacci.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <title>Fibonacci</title>
+        <meta charset="UTF-8">
+    </head>
+    
+        
+    <body>
+<?php
+$n=13;
+$a=0;
+$b=1;
+$soma;
+
+echo "$a";
+echo "$b";
+
+$soma=$a+$b;
+if ($soma<$n) {
+    echo "soma";
+    $a=$b;
+    $b=soma;
+    
+}
+else {
+    echo 'Fim';
+}
+?>
+</body>
+</html>
+
+
+
+        
+
+
